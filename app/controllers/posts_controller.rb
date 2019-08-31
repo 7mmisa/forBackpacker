a478748697c1eb00ba7d7d77b7940f1d011aac3a
粟野未早希 <awanomisaki@awanomisakinoMacBook-Air.local>
2019-08-31 17:22:52 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @posts = Post.new
+    @posts.images.build
+  end
+  
+  def create
+    
+    Post.create(post_params)
+  end
+
+  def show
+    @posts = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text, images_attributes: [:id, :image]).merge(user_id: 4, country_id: 1)
+    end
 end
